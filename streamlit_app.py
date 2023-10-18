98b4768167d4acf2633b94a0658ee54729851b9e
jimefuentes13 <64991271+jimefuentes13@users.noreply.github.com>
2023-10-18 02:49:32 -0600

    Update streamlit_app.py
    trying to get changes to show up

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Favorites')
+streamlit.header('Breakfast Favoritess')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
@@ -16,6 +16,5 @@ streamlit.dataframe(my_fruit_list)
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
