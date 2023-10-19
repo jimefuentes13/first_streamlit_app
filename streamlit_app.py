ad2545c264ad004b00478daf84943f59baabe8fc
jimefuentes13 <64991271+jimefuentes13@users.noreply.github.com>
2023-10-19 15:48:48 -0600

    Update streamlit_app.py
    Making api dependent on fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,10 +22,12 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to show API Response
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 #Api response
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 #Improving UX - normalizing 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
