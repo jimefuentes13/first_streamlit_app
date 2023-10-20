10b971202cec8024782e12a2f4590b8a7fe0cc65
jimefuentes13 <64991271+jimefuentes13@users.noreply.github.com>
2023-10-19 18:18:48 -0600

    Update streamlit_app.py
    removing kiwi

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,16 +28,16 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to show API Response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-#Api response
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-#Improving UX - normalizing 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Framing data in table
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information".)
+  else
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as 3:
+  streamlit.error()
 
 streamlit.stop()
 
